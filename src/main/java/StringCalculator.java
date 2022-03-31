d7d18b48c4ef16807e16fdc521581fa466725861
Mes2611 <hypercpp1@gmail.com>
2022-03-31 18:20:50 +0200

    StringCalculator step 4 method

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -6,9 +6,19 @@ import java.util.stream.Collectors;
 public class StringCalculator
 {
     public int add(String numbers) {
+        String defaultDelimeter = ",";
+        String newNumbers = "";
+        if(numbers.startsWith("//")) {
+            // récupération du delimiteur par défaut
+            defaultDelimeter = numbers.substring(2, numbers.indexOf("\n"));
+            newNumbers = numbers.substring(numbers.indexOf("\n") + 1, numbers.length());
+        }else {
+            newNumbers = numbers;
+        }
+        String delimeter = defaultDelimeter + "|\n";
         ArrayList<String> list = new ArrayList<>();
-        if(!numbers.isEmpty()) {
-           list.addAll(Arrays.stream(numbers.split(",|\n")).collect(Collectors.toList()));
+        if(!newNumbers.isEmpty()) {
+           list.addAll(Arrays.stream(newNumbers.split(delimeter)).collect(Collectors.toList()));
         return list.stream().mapToInt(Integer::parseInt).sum();
        }
         return 0;
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -6,27 +6,44 @@ public class StringCalculatorTest
     private StringCalculator stringCalculator = new StringCalculator();
 
     @Test
-    public void shouldReturn0ForNumbersIsEmpty() {
+    public void shouldReturn0ForNumbersIsEmpty()
+    {
         Assert.assertEquals(stringCalculator.add(""), 0);
     }
 
     @Test
-    public void shouldReturn1ForNumbersIs1() {
+    public void shouldReturn1ForNumbersIs1()
+    {
         Assert.assertEquals(stringCalculator.add("1"), 1);
     }
 
     @Test
-    public void shouldReturn3ForNumbersIs1And2() {
+    public void shouldReturn3ForNumbersIs1And2()
+    {
         Assert.assertEquals(stringCalculator.add("1,2"), 3);
     }
 
     @Test
-    public void shouldReturn26ForUnknowNumbers() {
+    public void shouldReturn26ForUnknowNumbers()
+    {
         Assert.assertEquals(stringCalculator.add("1,2,3,10,10"), 26);
     }
 
     @Test
-    public void shouldReturn6ForTwoDelimeter() {
+    public void shouldReturn6ForTwoDelimeter()
+    {
         Assert.assertEquals(stringCalculator.add("1\n2,3"), 6);
+    }
+
+    @Test
+    public void shouldReturn3ForAnyDelimeter()
+    {
+        Assert.assertEquals(stringCalculator.add("//;\n1;2"), 3);
+    }
+
+    @Test
+    public void shouldReturn6ForAnyDelimeter()
+    {
+        Assert.assertEquals(stringCalculator.add("//&\n1&2&3"), 6);
     }
 }
