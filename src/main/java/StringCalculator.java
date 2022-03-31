2a45e23df5784dc10c6ebc895b9a72a67218bbd6
Mes2611 <hypercpp1@gmail.com>
2022-03-31 11:06:15 +0200

    StringCalculator step 1 method and correction of the method signature Add to add

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,14 @@
 public class StringCalculator
 {
-    public int Add(String numbers) {
+    public int add(String numbers) {
+        if(numbers.length() == 1) {
+            return Integer.valueOf(numbers);
+        }
+
+        if(numbers.length() == 3) {
+            return Integer.valueOf(numbers.split(",")[0]) + Integer.valueOf(numbers.split(",")[1]);
+        }
+
         return 0;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -7,17 +7,17 @@ public class StringCalculatorTest
 
     @Test
     public void shouldReturn0ForNumbersIsEmpty() {
-        Assert.assertEquals(stringCalculator.Add(""), 0);
+        Assert.assertEquals(stringCalculator.add(""), 0);
     }
 
     @Test
     public void shouldReturn1ForNumbersIs1() {
-        Assert.assertEquals(stringCalculator.Add("1"), 1);
+        Assert.assertEquals(stringCalculator.add("1"), 1);
     }
 
     @Test
     public void shouldReturn3ForNumbersIs1And2() {
-        Assert.assertEquals(stringCalculator.Add("1,2"), 3);
+        Assert.assertEquals(stringCalculator.add("1,2"), 3);
     }
 
 }
